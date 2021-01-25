47d45c4c4392c957fc288d6f46cd421a99ee4e29
MCSpiderFe <felix-wagenknecht@web.de>
2021-01-25 18:23:16 +0100

    Added CPU struct

--- Emulator/main_6502.cpp
+++ Emulator/main_6502.cpp
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//reference:
+//http://www.obelisk.me.uk/6502/
+
+struct CPU
+{
+	using BYTE = unsigned char; //8 bit value
+	using WORD = unsigned short; //16 bit value
+
+	WORD PC; //Program Counter
+	WORD SP; //Stack Pointer
+
+	BYTE A, X, Y; //registers
+
+	//status flags
+	BYTE C : 1; //Carry Flag
+	BYTE Z : 1; //Zero Flag
+	BYTE I : 1; //Interrupt Disable
+	BYTE D : 1; //Decimal Mode
+	BYTE B : 1; //Break Command
+	BYTE V : 1; //Overflow Flag
+	BYTE N : 1; //Negative Flag
+
+};
+
 int main()
 {
-	printf("Hello 6502!");
+	CPU cpu;
 	return 0;
 }
