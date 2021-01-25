f59d0d5b5d710c7906efb7e10d8cf2df91c9c857
MCSpiderFe <felix-wagenknecht@web.de>
2021-01-25 18:32:35 +0100

    Added Memory struct and CPU boot routine

--- Emulator/main_6502.cpp
+++ Emulator/main_6502.cpp
@@ -4,11 +4,26 @@
 //reference:
 //http://www.obelisk.me.uk/6502/
 
+using BYTE = unsigned char; //8 bit value
+using WORD = unsigned short; //16 bit value
+
+using u32 = unsigned int;
+
+struct Mem
+{
+	static constexpr u32 MAX_MEM = 1024 * 64;
+	BYTE Data[MAX_MEM];
+
+	void Init()
+	{
+		for (u32 i = 0; i < MAX_MEM; i++) {
+			Data[i] = 0;
+		}
+	}
+};
+
 struct CPU
 {
-	using BYTE = unsigned char; //8 bit value
-	using WORD = unsigned short; //16 bit value
-
 	WORD PC; //Program Counter
 	WORD SP; //Stack Pointer
 
@@ -23,10 +38,20 @@ struct CPU
 	BYTE V : 1; //Overflow Flag
 	BYTE N : 1; //Negative Flag
 
+	void Reset(Mem& memory)
+	{
+		PC = 0xFFFC;
+		SP = 0x0100;
+		C = Z = I = D = B = V = N = 0;
+		A = X = Y = 0;
+		memory.Init();
+	}
 };
 
 int main()
 {
+	Mem mem;
 	CPU cpu;
+	cpu.Reset(mem);
 	return 0;
 }
